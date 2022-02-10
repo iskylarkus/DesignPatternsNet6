225f028f07281abadd7394ff5e8aa9a930566466
iskylarkus <turgay.kibar@gmail.com>
2022-02-10 15:08:38 +0300

    if necessary default identity users creating was implemented

--- BaseIdentity/Program.cs
+++ BaseIdentity/Program.cs
@@ -44,4 +44,26 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+
+
+using (var scope = app.Services.CreateScope())
+{
+    var identityDbContext = scope.ServiceProvider.GetRequiredService<AppIdentityDbContext>();
+
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<AppUser>>();
+
+    identityDbContext.Database.Migrate();
+
+    if (!userManager.Users.Any())
+    {
+        userManager.CreateAsync(new AppUser() { UserName = "user1", Email = "user1@hotmail.com" }, "Password12*").Wait();
+        userManager.CreateAsync(new AppUser() { UserName = "user2", Email = "user2@hotmail.com" }, "Password12*").Wait();
+        userManager.CreateAsync(new AppUser() { UserName = "user3", Email = "user3@hotmail.com" }, "Password12*").Wait();
+        userManager.CreateAsync(new AppUser() { UserName = "user4", Email = "user4@hotmail.com" }, "Password12*").Wait();
+        userManager.CreateAsync(new AppUser() { UserName = "user5", Email = "user5@hotmail.com" }, "Password12*").Wait();
+    }
+}
+
+
+
 app.Run();
