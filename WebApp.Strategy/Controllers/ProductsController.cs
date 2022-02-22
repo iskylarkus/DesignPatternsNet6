72b55410394ecd17b2cc8e88d18843c75bb34605
iskylarkus <turgay.kibar@gmail.com>
2022-02-22 12:38:32 +0300

    imported references modified

--- WebApp.Strategy/Controllers/ProductsController.cs
+++ WebApp.Strategy/Controllers/ProductsController.cs
@@ -1,17 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using BaseIdentity.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using BaseIdentity.Models;
 using WebApp.Strategy.Models;
 using WebApp.Strategy.Repositories;
-using Microsoft.AspNetCore.Identity;
 
 namespace WebApp.Strategy.Controllers
 {
+    [Authorize]
     public class ProductsController : Controller
     {
         private readonly IProductRepository _productRepository;
