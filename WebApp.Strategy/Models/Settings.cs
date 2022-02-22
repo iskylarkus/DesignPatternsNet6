800bf5d52cf47b7cb4af9eefc0dcddd3060c2432
iskylarkus <turgay.kibar@gmail.com>
2022-02-22 12:39:11 +0300

    settings controller and model developed

---
+++ WebApp.Strategy/Models/Settings.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApp.Strategy.Models
+{
+    public class Settings
+    {
+        public static string claimDatabaseType = "DatabaseType";
+
+        public EDatabaseType DatabaseType;
+
+        public EDatabaseType GetDefaultDatabaseType => EDatabaseType.SqlServer;
+    }
+}
