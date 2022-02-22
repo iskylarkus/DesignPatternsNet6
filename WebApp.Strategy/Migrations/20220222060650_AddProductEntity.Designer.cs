a8a0698423caf823c85e27066616da5d9c13fc06
iskylarkus <turgay.kibar@gmail.com>
2022-02-22 09:20:33 +0300

    migrating files created

--- WebApp.Strategy/Migrations/20220222060650_AddProductEntity.Designer.cs
+++ WebApp.Strategy/Migrations/20220222060650_AddProductEntity.Designer.cs
@@ -4,6 +4,7 @@ using BaseIdentity.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BaseIdentity.Migrations
 {
     [DbContext(typeof(AppIdentityDbContext))]
-    partial class AppIdentityDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220222060650_AddProductEntity")]
+    partial class AddProductEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
