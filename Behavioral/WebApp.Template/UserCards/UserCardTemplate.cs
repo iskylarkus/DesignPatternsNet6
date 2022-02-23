67769680bdb755447b35c3267ea274bb0aadc1da
iskylarkus <turgay.kibar@gmail.com>
2022-02-23 11:04:20 +0300

    user card template developed

---
+++ Behavioral/WebApp.Template/UserCards/UserCardTemplate.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using WebApp.Template.Models;
+
+namespace WebApp.Template.UserCards
+{
+    public abstract class UserCardTemplate
+    {
+        protected AppUser AppUser { get; set; }
+
+        protected UserCardTemplate(AppUser appUser)
+        {
+            AppUser = appUser;
+        }
+
+        public string Build()
+        {
+            if (AppUser == null) throw new ArgumentNullException(nameof(AppUser));
+
+            var sb = new StringBuilder();
+
+            sb.Append("<div class='card'>");
+            
+            sb.Append(SetPicture());
+
+            sb.Append($@"<div class='card-body'>
+                            <h5>{AppUser.UserName}</h5>
+                            <p>{AppUser.Description}</p>");
+            sb.Append(SetFooter());
+            sb.Append("</div>");
+
+            sb.Append("</div>");
+
+            return sb.ToString();
+        }
+
+        protected abstract string SetFooter();
+        protected abstract string SetPicture();
+    }
+}
