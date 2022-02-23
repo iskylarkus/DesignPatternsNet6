6219e25e591918a44953506338deb17b80d17e33
iskylarkus <turgay.kibar@gmail.com>
2022-02-23 11:04:05 +0300

    two special fields added to appuser

--- Behavioral/WebApp.Template/Models/AppUser.cs
+++ Behavioral/WebApp.Template/Models/AppUser.cs
@@ -4,5 +4,7 @@ namespace WebApp.Template.Models
 {
     public class AppUser : IdentityUser
     {
+        public string PictureUrl { get; set; }
+        public string Description { get; set; }
     }
 }
