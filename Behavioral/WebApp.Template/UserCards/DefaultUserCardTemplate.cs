45f1d820c203928f9879fa0384cc390f3d9108c7
iskylarkus <turgay.kibar@gmail.com>
2022-02-23 11:18:50 +0300

    default user template developed

---
+++ Behavioral/WebApp.Template/UserCards/DefaultUserCardTemplate.cs
@@ -0,0 +1,21 @@
+﻿using WebApp.Template.Models;
+
+namespace WebApp.Template.UserCards
+{
+    public class DefaultUserCardTemplate : UserCardTemplate
+    {
+        public DefaultUserCardTemplate(AppUser appUser) : base(appUser)
+        {
+        }
+
+        protected override string SetFooter()
+        {
+            return String.Empty;
+        }
+
+        protected override string SetPicture()
+        {
+            return $"<img class='card-img-top' src='/userpictures/blank.png'>";
+        }
+    }
+}
