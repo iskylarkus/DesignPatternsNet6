7939ac72fe10f18c4c4bf87d755fe97c70748697
iskylarkus <turgay.kibar@gmail.com>
2022-02-23 13:19:11 +0300

    two new columns picture and description added to data migration

--- Behavioral/WebApp.Template/Program.cs
+++ Behavioral/WebApp.Template/Program.cs
@@ -56,11 +56,11 @@ using (var scope = app.Services.CreateScope())
 
     if (!userManager.Users.Any())
     {
-        userManager.CreateAsync(new AppUser() { UserName = "user1", Email = "user1@hotmail.com" }, "Password12*").Wait();
-        userManager.CreateAsync(new AppUser() { UserName = "user2", Email = "user2@hotmail.com" }, "Password12*").Wait();
-        userManager.CreateAsync(new AppUser() { UserName = "user3", Email = "user3@hotmail.com" }, "Password12*").Wait();
-        userManager.CreateAsync(new AppUser() { UserName = "user4", Email = "user4@hotmail.com" }, "Password12*").Wait();
-        userManager.CreateAsync(new AppUser() { UserName = "user5", Email = "user5@hotmail.com" }, "Password12*").Wait();
+        userManager.CreateAsync(new AppUser() { UserName = "user1", Email = "user1@hotmail.com", PictureUrl = "/userpictures/exists.jpg", Description = "Lorem Ipsum, dizgi ve baský endüstrisinde kullanýlan mýgýr metinlerdir. Lorem Ipsum, adý bilinmeyen bir matbaacýnýn bir hurufat numune kitabý oluþturmak üzere bir yazý galerisini alarak karýþtýrdýðý 1500'lerden beri endüstri standardý sahte metinler olarak kullanýlmýþtýr." }, "Password12*").Wait();
+        userManager.CreateAsync(new AppUser() { UserName = "user2", Email = "user2@hotmail.com", PictureUrl = "/userpictures/exists.jpg", Description = "Lorem Ipsum, dizgi ve baský endüstrisinde kullanýlan mýgýr metinlerdir. Lorem Ipsum, adý bilinmeyen bir matbaacýnýn bir hurufat numune kitabý oluþturmak üzere bir yazý galerisini alarak karýþtýrdýðý 1500'lerden beri endüstri standardý sahte metinler olarak kullanýlmýþtýr." }, "Password12*").Wait();
+        userManager.CreateAsync(new AppUser() { UserName = "user3", Email = "user3@hotmail.com", PictureUrl = "/userpictures/exists.jpg", Description = "Lorem Ipsum, dizgi ve baský endüstrisinde kullanýlan mýgýr metinlerdir. Lorem Ipsum, adý bilinmeyen bir matbaacýnýn bir hurufat numune kitabý oluþturmak üzere bir yazý galerisini alarak karýþtýrdýðý 1500'lerden beri endüstri standardý sahte metinler olarak kullanýlmýþtýr." }, "Password12*").Wait();
+        userManager.CreateAsync(new AppUser() { UserName = "user4", Email = "user4@hotmail.com", PictureUrl = "/userpictures/exists.jpg", Description = "Lorem Ipsum, dizgi ve baský endüstrisinde kullanýlan mýgýr metinlerdir. Lorem Ipsum, adý bilinmeyen bir matbaacýnýn bir hurufat numune kitabý oluþturmak üzere bir yazý galerisini alarak karýþtýrdýðý 1500'lerden beri endüstri standardý sahte metinler olarak kullanýlmýþtýr." }, "Password12*").Wait();
+        userManager.CreateAsync(new AppUser() { UserName = "user5", Email = "user5@hotmail.com", PictureUrl = "/userpictures/exists.jpg", Description = "Lorem Ipsum, dizgi ve baský endüstrisinde kullanýlan mýgýr metinlerdir. Lorem Ipsum, adý bilinmeyen bir matbaacýnýn bir hurufat numune kitabý oluþturmak üzere bir yazý galerisini alarak karýþtýrdýðý 1500'lerden beri endüstri standardý sahte metinler olarak kullanýlmýþtýr." }, "Password12*").Wait();
     }
 }
 
