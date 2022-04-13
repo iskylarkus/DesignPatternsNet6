f269e1154203eaad2d88b23c55b2e98a0e1158ec
iskylarkus <turgay.kibar@gmail.com>
2022-04-13 17:33:33 +0300

    ExcelFile class developed

---
+++ Behavioral/WebApp.Command/Commands/ExcelFile.cs
@@ -0,0 +1,53 @@
+﻿using ClosedXML.Excel;
+using System.Data;
+
+namespace WebApp.Command.Commands
+{
+    public class ExcelFile<T>
+    {
+        public readonly List<T> _list;
+
+        public string FileName => $"{typeof(T).FullName}.xlsx";
+        public string FileType => "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+
+        public ExcelFile(List<T> list)
+        {
+            _list = list;
+        }
+
+        public MemoryStream Create()
+        {
+            var wb = new XLWorkbook();
+
+            var ds = new DataSet();
+
+            ds.Tables.Add(GetTable());
+
+            wb.Worksheets.Add(ds);
+
+            var excelMemory = new MemoryStream();
+
+            wb.SaveAs(excelMemory);
+
+            return excelMemory;
+        }
+
+        private DataTable GetTable()
+        {
+            var table = new DataTable();
+
+            var type = typeof(T);
+
+            type.GetProperties().ToList().ForEach(p => table.Columns.Add(p.Name, p.PropertyType));
+
+            _list.ForEach(x =>
+            {
+                var values = type.GetProperties().Select(propertyInfo => propertyInfo.GetValue(x, null)).ToArray();
+
+                table.Rows.Add(values);
+            }) ;
+
+            return table;
+        }
+    }
+}
