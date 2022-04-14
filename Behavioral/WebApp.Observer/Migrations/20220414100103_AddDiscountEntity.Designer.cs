9888a601498bd20bc2427f389fb6e57aa774ee8d
iskylarkus <turgay.kibar@gmail.com>
2022-04-14 13:02:45 +0300

    Add-Migration AddDiscountEntity

--- Behavioral/WebApp.Observer/Migrations/20220414100103_AddDiscountEntity.Designer.cs
+++ Behavioral/WebApp.Observer/Migrations/20220414100103_AddDiscountEntity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebApp.Observer.Models;
 
@@ -11,9 +12,10 @@ using WebApp.Observer.Models;
 namespace WebApp.Observer.Migrations
 {
     [DbContext(typeof(AppIdentityDbContext))]
-    partial class AppIdentityDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220414100103_AddDiscountEntity")]
+    partial class AddDiscountEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
