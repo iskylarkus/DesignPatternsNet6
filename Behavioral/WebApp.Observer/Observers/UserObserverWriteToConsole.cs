464b037d52b04423c2d95c126f9c1e6318ccbf3d
iskylarkus <turgay.kibar@gmail.com>
2022-04-15 16:41:25 +0300

    UserObserverWriteToConsole implemented

---
+++ Behavioral/WebApp.Observer/Observers/UserObserverWriteToConsole.cs
@@ -0,0 +1,21 @@
+﻿using WebApp.Observer.Models;
+
+namespace WebApp.Observer.Observers
+{
+    public class UserObserverWriteToConsole : IUserObserver
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public UserObserverWriteToConsole(IServiceProvider serviceProvider)
+        {
+            this._serviceProvider = serviceProvider;
+        }
+
+        public void CreateUser(AppUser appUser)
+        {
+            var logger = _serviceProvider.GetRequiredService<ILogger<UserObserverWriteToConsole>>();
+
+            logger.LogInformation($"user created : id = {appUser.Id}");
+        }
+    }
+}
