0d7abcc1b4b3ba7ec6d82bcdc7f06d7f4d6632bc
iskylarkus <turgay.kibar@gmail.com>
2022-04-16 10:19:24 +0300

    UserObserverCreateDiscount implemented

---
+++ Behavioral/WebApp.Observer/Observers/UserObserverCreateDiscount.cs
@@ -0,0 +1,27 @@
+﻿using WebApp.Observer.Models;
+
+namespace WebApp.Observer.Observers
+{
+    public class UserObserverCreateDiscount : IUserObserver
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public UserObserverCreateDiscount(IServiceProvider serviceProvider)
+        {
+            this._serviceProvider = serviceProvider;
+        }
+
+        public void CreateUser(AppUser appUser)
+        {
+            var logger = _serviceProvider.GetRequiredService<ILogger<UserObserverCreateDiscount>>();
+
+            var context = _serviceProvider.GetRequiredService<AppIdentityDbContext>();
+
+            context.Discounts.Add(new Discount { Rate = 10, UserId = appUser.Id });
+
+            context.SaveChanges();
+
+            logger.LogInformation($"discount created for user id : {appUser.Id}");
+        }
+    }
+}
